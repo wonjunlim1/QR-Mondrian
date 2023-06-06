656dcb32b1fc03f3cb51a9a604f5cae7adfc48aa
Wonjun Lim <wlim2024@chadwickschool.org>
2023-06-06 14:17:32 +0900

    FE: updated app.js to meet file structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,66 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './LoginPage';  // Assuming LoginPage is in the same directory
-import OrderPage from './OrderPage';  // Assuming OrderPage is in the same directory
+import React, {useEffect} from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation} from 'react-router-dom';
+
+import WebLoginPage from './Pages/Web/LoginPage';  
+import WebOrderPage from './Pages/Web/OrderPage';  
+import MobileMenuPage from './Pages/Mobile/MenuPage';
+
+function PageHandler() {
+  const location = useLocation();
+  const { pathname, key } = location;
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [key]);
+
+  useEffect(() => {
+    let title = "";
+    let metaDescription = "";
+
+    switch (pathname) {
+      case "/menu_m/:restaurant_id/:branch_id/:table_number":
+        title = "Menu";
+        metaDescription = "This is the Mobile Menu Page";
+        break;
+      case "/login_w":
+        title = "Login";
+        metaDescription = "This is the Web Login Page";
+        break;
+      case "/order_w/:restaurant_id":
+        title = "";
+        metaDescription = "";
+        break;
+      default:
+          title = "SPQR";
+          metaDescription = "";
+          break;
+    }
+
+    if (title) {
+      document.title = title;
+    }
+
+    if (metaDescription) {
+      const metaDescriptionTag = document.querySelector(
+        'head > meta[name="description"]'
+      );
+      if (metaDescriptionTag) {
+        metaDescriptionTag.content = metaDescription;
+      }
+    }
+  }, [pathname]);
+
+}
+
 
 function App() {
   return (
     <Router>
+      <PageHandler />
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/order" element={<OrderPage />} />
+        <Route path="/login_w" element={<WebLoginPage />} />
+        <Route path="/order_w" element={<WebOrderPage />} />
+        <Route path="/menu_m/:restaurant_id/:branch_id/:table_number" element={<MobileMenuPage/>}/>
       </Routes>
     </Router>
   );

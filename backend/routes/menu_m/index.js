1785e340fe563ea332b4a668da80e70b6475e12f
Wonjun Lim <wlim2024@chadwickschool.org>
2023-06-06 14:11:02 +0900

    BE: updated routes

--- backend/routes/menu_m/index.js
+++ backend/routes/menu_m/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const mobileMenuController = require('../../controller/mobileMenuController')
 
-/** Get all menu for restaurant_id > branch_id > :table_number*/
+/** Get all menu for restaurant_id > branch_id > :table_number */
 router.get('/:restaurant_id/:branch_id/:table_number', mobileMenuController.getMobileMenu)
+
+/** Get menu details for restaurant_id > branch_id > :table_number > :menu_id */
+router.get('/:restaurant_id/:branch_id/:table_number/:menu_id', mobileMenuController.getMobileMenuDetail)
 
 module.exports = router;

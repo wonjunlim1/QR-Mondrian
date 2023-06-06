e034c8d15f58563eb5d1ccd2dac36927a9d338be
Wonjun Lim <wlim2024@chadwickschool.org>
2023-06-06 14:02:21 +0900

    BE: set up port number for BE

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -34,7 +34,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -50,6 +50,14 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+const server = app.listen(8080, () => {
+  // const address = server.address();
+  // const host = address.address;
+  // const port = address.port;
+  // console.log(`Server is running at http://${host}:${port}`);
+  console.log('Ready on port %d', server.address().port);
 });
 
 module.exports = app;

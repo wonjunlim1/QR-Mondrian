288ae89529ffbd6ac97c4bb6a35732f5ca26edac
Wonjun Lim <wlim2024@chadwickschool.org>
2023-06-12 20:16:53 +0900

    BE: Editted CORS Settings for Security Reasons

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Enable CORS middleware for frontend at all ports
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();

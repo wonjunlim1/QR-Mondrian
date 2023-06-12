11e4b90fa7f718b0e046a2b89e1c11abeabc7b12
Wonjun Lim <wlim2024@chadwickschool.org>
2023-06-12 19:44:01 +0900

    BE: Started POST Request for Mobile Cart

--- backend/service/mobileCartService.js
+++ backend/service/mobileCartService.js
@@ -77,4 +77,44 @@ module.exports = {
       throw error;
     }
   },
+  /* Post current order */
+  postCurrentOrder: async (
+    restaurant_id,
+    branch_id,
+    table_number,
+    current_order
+  ) => {
+    const curr_order = current_order.current_order;
+    let status = 1;
+
+    // Calculate the sum of the current_order
+    let currentOrderPrice = 0;
+    if (curr_order && Array.isArray(curr_order)) {
+      for (const item of curr_order) {
+        if (item.main_menus && Array.isArray(item.main_menus)) {
+          for (const menu of item.main_menus) {
+            currentOrderPrice += menu.price;
+
+            if (menu.option_menus && Array.isArray(menu.option_menus)) {
+              for (const optionMenu of menu.option_menus) {
+                currentOrderPrice += optionMenu.price;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    const pastOrders = await Order.findAll({
+      where: {
+        restaurant_id,
+        branch_id,
+        table_number,
+      },
+      order: [["created_at", "DESC"]],
+      limit: 1,
+    });
+    // console.log(status)
+    // return {'status' : status};
+  },
 };

2c79ef29ea7ae2509dd44863f93701f4ab725ee8
Wonjun Lim <wlim2024@chadwickschool.org>
2023-06-27 12:19:15 +0900

    FE: Added Retrieving User Attributes in LoginPage

--- frontend/src/Pages/Web/LoginPage.js
+++ frontend/src/Pages/Web/LoginPage.js
@@ -1,22 +1,32 @@
-import React, { useState } from 'react'; 
-import jwt_decode from 'jwt-decode';
-import { CognitoUser, AuthenticationDetails, CognitoUserPool } from 'amazon-cognito-identity-js';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import jwt_decode from "jwt-decode";
+import {
+  CognitoUser,
+  AuthenticationDetails,
+  CognitoUserPool,
+} from "amazon-cognito-identity-js";
+import { useNavigate } from "react-router-dom";
 
-import styles from './LoginPage.module.css';
-import logo_image from '../../Assets/Images/qr_logo.png';
+import styles from "./LoginPage.module.css";
+import logoImage from "../../Assets/Images/qr_logo.png";
 
 function LoginPage(props) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const userPoolId = process.env.REACT_APP_USER_POOL_ID;
-  const clientId = process.env.REACT_APP_CLIENT_ID;
+  // Navigation and location utility from React Router
   const navigate = useNavigate();
 
+  // Initializing states
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Cognito variable assignments
+  const userPoolId = process.env.REACT_APP_USER_POOL_ID;
+  const clientId = process.env.REACT_APP_CLIENT_ID;
+
+  /** Event Handlers */
+
+  // Function to hangle logins
   const handleLogin = (event) => {
     event.preventDefault();
-    console.log('hello');
     const userPool = new CognitoUserPool({
       UserPoolId: userPoolId,
       ClientId: clientId,
@@ -35,44 +45,62 @@ function LoginPage(props) {
     user.authenticateUser(authDetails, {
       onSuccess: (result) => {
         const decodedToken = jwt_decode(result.getAccessToken().getJwtToken());
-        const isHQUser = decodedToken['cognito:groups'].includes('headquarters')
-        navigate('/order_w',  { state: { isHQUser } });
-       
+        const isHQUser =
+          decodedToken["cognito:groups"].includes("headquarters");
+        const isBranchUser = decodedToken["cognito:groups"].includes("branch");
+
+        user.getUserAttributes((err, attributes) => {
+          if (err) {
+            console.error(err);
+          } else {
+            const nameAttribute = attributes.find(
+              (attribute) => attribute.getName() === "name"
+            );
+            const identifier = nameAttribute ? nameAttribute.getValue() : null;
+            const [restaurantId, branchId] = identifier.split("/");
+            if (isHQUser) {
+              navigate(`/menu_w/${restaurantId}/${branchId}`, {
+                state: { isHQUser },
+              });
+            } else if (isBranchUser) {
+              navigate(`/order_w/${restaurantId}/${branchId}`, {
+                state: { isBranchUser },
+              });
+            }
+          }
+        });
       },
       onFailure: (err) => {
         console.error(err);
-        // TODO: handle failed sign in, e.g. show error message to user
       },
     });
   };
 
+  // Render the component
   return (
     <div className={styles.loginPage}>
-      <img src={logo_image} alt="Logo" className={styles.logoImage} /> 
-      <div className={styles.fieldWrapper}>
+      <img src={logoImage} alt="Logo" className={styles.logoImage} />
+      <form onSubmit={handleLogin} className={styles.fieldWrapper}>
         <p className={styles.inputLabel}>ID</p>
-        <input 
-          type="text" 
+        <input
+          type="text"
           className={styles.inputField}
-          value={username} 
-          onChange={(e) => setUsername(e.target.value)} 
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
         <p className={styles.inputLabel}>비밀번호</p>
-        <input 
-          type="password" 
+        <input
+          type="password"
           className={styles.inputField}
-          value={password} 
-          onChange={(e) => setPassword(e.target.value)} 
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
-      </div>
-      <button  
-        onClick={handleLogin}
-        className={styles.styledButton}
-      >
-        로그인
-      </button>
+        <button onClick={handleLogin} className={styles.styledButton}>
+          로그인
+        </button>
+      </form>
     </div>
   );
-};
+}
 
 export default LoginPage;
--- frontend/src/Pages/Web/OrderPage.js
+++ frontend/src/Pages/Web/OrderPage.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React from "react";
+import { useLocation } from "react-router-dom";
 
 function OrderPage(props) {
-    const location = useLocation();
-  
-    // If the state was passed in the route, use it, otherwise default to false
-    const isHQUser = location.state ? location.state.isHQUser : false;
-  
-    return (
-      <div>
-        {isHQUser ? (
-          <div>
-            <h1>Welcome, HQ!</h1>
-          </div>
-        ) : (
-          <div>
-            <h1>Welcome, Outlet!</h1>
-          </div>
-        )}
-      </div>
-    );
+  const location = useLocation();
+
+  // If the state was passed in the route, use it, otherwise default to false
+  const isHQUser = location.state ? location.state.isHQUser : false;
+  //const isBranchUser = location.state ? location.state.isBranchUser : false;
+
+  return (
+    <div>
+      {isHQUser ? (
+        <div>
+          <h1>Welcome, HQ!</h1>
+        </div>
+      ) : (
+        <div>
+          <h1>Welcome, Outlet!</h1>
+        </div>
+      )}
+    </div>
+  );
 }
-  
 
 export default OrderPage;

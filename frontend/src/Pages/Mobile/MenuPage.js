b78607897b029413ac42cfdd35173bc5c32415de
Wonjun Lim <wlim2024@chadwickschool.org>
2023-07-19 00:21:30 +0900

    FE: Implemeted API Call for MenuPage

--- frontend/src/Pages/Mobile/MenuPage.js
+++ frontend/src/Pages/Mobile/MenuPage.js
@@ -1,248 +1,168 @@
-import { useCallback, useEffect, useState, useRef } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-
+import React from "react";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams, useLocation } from "react-router-dom";
+import WebHeader from "../../Components/WebHeader";
+import { decryptUrlParams } from "../../utils/encryption";
 import styles from "./MenuPage.module.css";
-import cartIcon from "../../Assets/Images/cart-white.svg";
-import { encryptUrlParams, decryptUrlParams } from "../../utils/encryption";
+import deleteIcon from "../../Assets/Images/delete.svg";
+import dragIcon from "../../Assets/Images/drag.svg";
+import editIcon from "../../Assets/Images/edit.svg";
+import pauseIcon from "../../Assets/Images/pause.svg";
+import playIcon from "../../Assets/Images/play.svg";
+import plusIcon from "../../Assets/Images/plus-white.svg";
 
 const MenuPage = () => {
-  // Navigation utility from React Router
+  // Navigation and location utility from React Router
   const navigate = useNavigate();
+  const location = useLocation();
 
   //Server address variable assignment
   const serverAddress = process.env.REACT_APP_SERVER_ADDRESS;
 
   // Initializing states
   const [menuData, setMenuData] = useState(null);
-  const [selectedCategory, setSelectedCategory] = useState(0);
-  const [isAutomaticScroll, setIsAutomaticScroll] = useState(false);
-  const menuCategoryRefs = useRef([]);
-  const categoryPillRefs = useRef([]);
+
+  // If the state was passed in the route, use it, otherwise default to false
+  const isHQUser = location.state ? location.state.isHQUser : false;
+  const isBranchUser = location.state ? location.state.isBranchUser : false;
 
   // Extracting params from URL
-  const {
-    restaurant_id: encodedRestaurantId,
-    branch_id: encodedBranchId,
-    table_number: encodedTableNumber,
-  } = useParams();
+  const { restaurant_id: encodedRestaurantId, branch_id: encodedBranchId } =
+    useParams();
 
   // Decoding params
   const restaurantId = decryptUrlParams(encodedRestaurantId);
   const branchId = decryptUrlParams(encodedBranchId);
-  const tableNumber = decryptUrlParams(encodedTableNumber);
-
-  // Path for restaruant logo image
-  const restaurantLogoImage = `https://spqr-menu.s3.ap-northeast-2.amazonaws.com/${restaurantId}/logo.jpg`;
-
-  /** Event Handlers */
-
-  // Function to handle click on menu item
-  const onMenuClick = useCallback(
-    (id) => {
-      navigate(
-        `/menu_m/${encryptUrlParams(restaurantId)}/${encryptUrlParams(
-          branchId
-        )}/${encryptUrlParams(tableNumber)}/${encryptUrlParams(id)}`
-      );
-    },
-    [navigate, restaurantId, branchId, tableNumber]
-  );
-
-  // Function to handle click on cart icon
-  const onCartIconClick = useCallback(() => {
-    navigate(
-      `/cart_m/${encryptUrlParams(restaurantId)}/${encryptUrlParams(
-        branchId
-      )}/${encryptUrlParams(tableNumber)}`
-    );
-  }, [navigate, restaurantId, branchId, tableNumber]);
-
-  // Function to create a Promise that resolves when automatic scrolling ends
-  const smoothScrollTo = (y) =>
-    new Promise((resolve) => {
-      const onScroll = () => {
-        if (window.pageYOffset === y) {
-          window.removeEventListener("scroll", onScroll);
-          resolve();
-        }
-      };
-      window.addEventListener("scroll", onScroll);
-      window.scrollTo({ top: y, behavior: "smooth" });
-    });
-
-  // Function to handle clicks on category pills
-  const onCategoryClick = async (index) => {
-    setIsAutomaticScroll(true); // Set manual scroll to false
-    const y = menuCategoryRefs.current[index].offsetTop - 183;
-    await smoothScrollTo(y);
-    setIsAutomaticScroll(false);
-    setSelectedCategory(index);
-    let temp_index = index;
-    if (index >= 1) {
-      temp_index = index - 1;
-    }
-    categoryPillRefs.current[temp_index].scrollIntoView({
-      behavior: "smooth",
-      block: "nearest",
-      inline: "center",
-    });
-  };
+  const dummyTableNumber = 0;
 
   /** Effect Hooks */
+
+  // Effect to redirect user if not logged in
+  useEffect(() => {
+    if (!isHQUser && !isBranchUser) {
+      navigate(`/login_w`);
+    }
+  }, [navigate, isHQUser, isBranchUser, restaurantId, branchId]);
 
   // Effect to fetch menu data once the component mounts
   useEffect(() => {
     const fetchMenuData = async () => {
       try {
         const response = await fetch(
-          `${serverAddress}/menu/${restaurantId}/${branchId}/${tableNumber}`
+          `${serverAddress}/menu/${restaurantId}/${branchId}/${dummyTableNumber}`
         );
         const jsonData = await response.json();
-        let updatedData = jsonData.data.menu.map((category) => ({
-          ...category,
-          main_menus: category.main_menus.filter(
-            (menu) => menu.menu_status === true
-          ),
-        }));
-        console.log(updatedData);
-        const finalData = updatedData.filter(
-          (category) => category.main_menus.length > 0
-        );
-        setMenuData(finalData);
+        setMenuData(jsonData.data.menu);
       } catch (error) {
         console.log("Error fetching menu data:", error);
       }
     };
 
     fetchMenuData();
-  }, [serverAddress, restaurantId, branchId, tableNumber]);
-
-  // Effect to enable highlight change while user scrolls
-  useEffect(() => {
-    if (isAutomaticScroll) return;
-    const handleScroll = () => {
-      const { current } = menuCategoryRefs;
-      const scrollTop =
-        window.pageYOffset || document.documentElement.scrollTop;
-
-      const index = current.findIndex(
-        (categoryRef) =>
-          scrollTop >= categoryRef.offsetTop - 183 &&
-          scrollTop < categoryRef.offsetTop - 183 + categoryRef.offsetHeight
-      );
-
-      if (index !== -1) {
-        setSelectedCategory(index);
-        let temp_index = index;
-        if (index >= 1) {
-          temp_index = index - 1;
-        }
-        categoryPillRefs.current[temp_index].scrollIntoView({
-          behavior: "instant",
-          inline: "center",
-        });
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [isAutomaticScroll]);
+  }, [serverAddress, restaurantId, branchId, dummyTableNumber]);
 
   // Return null while data is loading
   if (!menuData) {
     return <div></div>;
   }
 
-  // Render the component
   return (
-    <div className={styles.mobile}>
-      <div className={styles.headerFixed}>
-        <div className={styles.headerSpace}></div>
-        <div className={styles.header}>
-          <div className={styles.restaurantLogo}>
-            <img
-              className={styles.restaurantLogoChild}
-              alt=""
-              src={restaurantLogoImage}
-            />
-          </div>
-          <div className={styles.tableNumberWrapper}>
-            <b className={styles.tableNumberLabel}>테이블 번호</b>
-            <b className={styles.tableNumber}>{tableNumber}</b>
-          </div>
-          <div className={styles.icon} onClick={() => onCartIconClick()}>
-            <img className={styles.iconChild} alt="" src={cartIcon} />
-          </div>
-        </div>
-      </div>
-      <div className={styles.categoryScrollBarWrapperFixed}>
-        <div className={styles.categoryScrollBarWrapper}>
-          <div className={styles.categoryScrollBar}>
-            {menuData.map((menuCategory, index) => (
-              <div
-                key={menuCategory.category_name}
-                onClick={() => onCategoryClick(index)}
-                className={
-                  selectedCategory === index
-                    ? styles.categoryPillSelected
-                    : styles.categoryPill
-                }
-                ref={(el) => (categoryPillRefs.current[index] = el)}
-              >
-                <div className={styles.menuCategoryLabel}>
-                  {menuCategory.category_name}
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-      <div className={styles.layout}>
-        {menuData.map((menuCategory, index) => (
-          <div
-            key={menuCategory.category_name}
-            className={styles.menuCategoryWrapper}
-            ref={(el) => (menuCategoryRefs.current[index] = el)}
-          >
-            <div className={styles.categoryTitleWrapper}>
-              <b className={styles.categoryTitle}>
-                {menuCategory.category_name}
-              </b>
+    <>
+      <div className={styles.web}>
+        <WebHeader
+          isHQUser={isHQUser}
+          isBranchUser={isBranchUser}
+          restaurantId={restaurantId}
+          branchId={branchId}
+        />
+        <div className={styles.contentWrapper} id="current_menu_body">
+          <div className={styles.titlearea} id="title_div">
+            <div className={styles.title}>
+              <h1 className={styles.title1}>현재 메뉴</h1>
             </div>
-            {menuCategory.main_menus.map((menu) => (
-              <div
-                key={menu.id}
-                className={styles.menuItemWrapper}
-                onClick={() => onMenuClick(menu.id)}
-              >
-                {menu.image_url && (
-                  <img
-                    className={styles.menuImage}
-                    alt={menu.name}
-                    loading="lazy"
-                    src={menu.image_url}
-                  />
-                )}
-                <div className={styles.menuContent}>
-                  <b className={styles.menuName}>{menu.name}</b>
-                  {menu.description && (
-                    <div className={styles.menuDescription}>
-                      {menu.description}
-                    </div>
-                  )}
-                  <b className={styles.menuPrice}>
-                    {menu.price.toLocaleString()}원
-                  </b>
-                </div>
-              </div>
-            ))}
+            <button className={styles.button}>
+              <div className={styles.label}>메뉴 순서 변경</div>
+            </button>
           </div>
-        ))}
+          {menuData.map((menuCategory) => (
+            <div
+              key={menuCategory.category_name}
+              className={styles.layout}
+              id="category_card"
+            >
+              <div className={styles.titlearea1} id="category_card_title">
+                <h2 className={styles.title1}>{menuCategory.category_name}</h2>
+                <button className={styles.icon3}>
+                  <button className={styles.xS}>
+                    <img alt="" src={deleteIcon} />
+                  </button>
+                </button>
+              </div>
+
+              {menuCategory.main_menus.map((menuItem) => (
+                <div
+                  key={menuItem.id}
+                  className={styles.row}
+                  id="menu_card_row"
+                >
+                  <div className={styles.menuCard} id="menu_card_body">
+                    <div className={styles.layout1} id="menu_card_buttons">
+                      <div className={styles.icongroup}>
+                        <button className={styles.icon3}>
+                          <button className={styles.xS}>
+                            <img alt="" src={editIcon} />
+                          </button>
+                        </button>
+                        <button className={styles.icon3}>
+                          <button className={styles.xS}>
+                            <img alt="" src={pauseIcon} />
+                          </button>
+                        </button>
+                        <button className={styles.icon3}>
+                          <button className={styles.xS}>
+                            <img alt="" src={deleteIcon} />
+                          </button>
+                        </button>
+                      </div>
+                    </div>
+
+                    <div className={styles.layout2} id="menu_card">
+                      <img
+                        className={styles.menuimageIcon}
+                        alt=""
+                        src={menuItem.image_url}
+                      />
+
+                      <div className={styles.menuinfo} id="menu_card_text">
+                        <div className={styles.div}>
+                          <h1 className={styles.menuname} id="menu_name">
+                            {menuItem.name}
+                          </h1>
+
+                          <h2 className={styles.description} id="menu_explain">
+                            {menuItem.description}
+                          </h2>
+                        </div>
+
+                        <h2 className={styles.value1} id="menu_cost">
+                          {menuItem.price}원
+                        </h2>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+        <footer className={styles.layout22} id="current_menu_down">
+          <button className={styles.floatingbutton}>
+            <div className={styles.floatingbuttonChild} />
+            <img className={styles.plusLIcon} alt="" src={plusIcon} />
+          </button>
+        </footer>
       </div>
-    </div>
+    </>
   );
 };
 

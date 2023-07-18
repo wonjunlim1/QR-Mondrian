eb5bfaa73ecc286c24d58a8110099f0dd2a6cc41
Wonjun Lim <wlim2024@chadwickschool.org>
2023-07-18 23:33:16 +0900

    BE: Rearranged Routes for Menu_w index.js

--- backend/routes/menu_w/index.js
+++ backend/routes/menu_w/index.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const webController = require("../../controller/webController");
 
 /** Put menu status for restaurant_id > branch_id */
-router.put("/:restaurant_id/:branch_id/", webController.putMenuStatus);
+router.put("/:restaurant_id/:branch_id/status", webController.putMenuStatus);
 
 /** Delete menu or category for restaurant_id > branch_id */
 router.delete("/:restaurant_id/:branch_id/:request_type/:request_id", webController.deleteMenuCategory);
 
 /** Post new menu category for restaurant_id > branch_id */
-router.post("/:restaurant_id/:branch_id/edit", webController.createMenuCategory);
+router.post("/:restaurant_id/:branch_id/category", webController.createMenuCategory);
 
 /** Put menu & menu category display orders for restaurant_id > branch_id */
-router.put("/:restaurant_id/:branch_id/edit", webController.putDisplayOrder);
+router.put("/:restaurant_id/:branch_id/display_order", webController.putDisplayOrder);
 
 module.exports = router;

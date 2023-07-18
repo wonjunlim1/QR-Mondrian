08eb88cdb0e838e6a76fa1497528570763d52eec
Wonjun Lim <wlim2024@chadwickschool.org>
2023-07-19 00:20:25 +0900

    FE: Changed Logo Image for LoginPage

--- frontend/src/Pages/Web/LoginPage.js
+++ frontend/src/Pages/Web/LoginPage.js
@@ -7,7 +7,7 @@ import {
 } from "amazon-cognito-identity-js";
 import { useNavigate } from "react-router-dom";
 import styles from "./LoginPage.module.css";
-import logoImage from "../../Assets/Images/qr_logo.png";
+import logoImage from "../../Assets/Images/logo.jpeg";
 import { encryptUrlParams } from "../../utils/encryption";
 
 function LoginPage() {

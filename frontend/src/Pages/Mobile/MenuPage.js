a5a72e5acf4145fbb78f0da215dcb360b55d187f
Wonjun Lim <wlim2024@chadwickschool.org>
2023-07-19 00:22:42 +0900

    FE: Implemented Menu Status Update for MenuPage

--- frontend/src/Pages/Mobile/MenuPage.js
+++ frontend/src/Pages/Mobile/MenuPage.js
@@ -5,11 +5,11 @@ import WebHeader from "../../Components/WebHeader";
 import { decryptUrlParams } from "../../utils/encryption";
 import styles from "./MenuPage.module.css";
 import deleteIcon from "../../Assets/Images/delete.svg";
-import dragIcon from "../../Assets/Images/drag.svg";
 import editIcon from "../../Assets/Images/edit.svg";
 import pauseIcon from "../../Assets/Images/pause.svg";
 import playIcon from "../../Assets/Images/play.svg";
 import plusIcon from "../../Assets/Images/plus-white.svg";
+//import dragIcon from "../../Assets/Images/drag.svg";
 
 const MenuPage = () => {
   // Navigation and location utility from React Router
@@ -21,6 +21,7 @@ const MenuPage = () => {
 
   // Initializing states
   const [menuData, setMenuData] = useState(null);
+  const [eventCounter, setEventCounter] = useState(0);
 
   // If the state was passed in the route, use it, otherwise default to false
   const isHQUser = location.state ? location.state.isHQUser : false;
@@ -34,6 +35,34 @@ const MenuPage = () => {
   const restaurantId = decryptUrlParams(encodedRestaurantId);
   const branchId = decryptUrlParams(encodedBranchId);
   const dummyTableNumber = 0;
+
+  /** Event Handlers */
+
+  // Function to handle click on button
+  const onStatusButtonClick = async (id, state) => {
+    const stateNum = state ? 0 : 1;
+    const data = {
+      menu_status_change: {
+        menu_id: id,
+        status: stateNum,
+      },
+    };
+    try {
+      const response = await fetch(
+        `${serverAddress}/menu_w/${restaurantId}/${branchId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+      setEventCounter(eventCounter + 1);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   /** Effect Hooks */
 
@@ -59,7 +88,7 @@ const MenuPage = () => {
     };
 
     fetchMenuData();
-  }, [serverAddress, restaurantId, branchId, dummyTableNumber]);
+  }, [serverAddress, restaurantId, branchId, dummyTableNumber, eventCounter]);
 
   // Return null while data is loading
   if (!menuData) {
@@ -76,91 +105,157 @@ const MenuPage = () => {
           branchId={branchId}
         />
         <div className={styles.contentWrapper} id="current_menu_body">
-          <div className={styles.titlearea} id="title_div">
-            <div className={styles.title}>
-              <h1 className={styles.title1}>현재 메뉴</h1>
+          <div className={styles.titleArea} id="title_div">
+            <div className={styles.titleWrapper}>
+              <h1 className={styles.titleLabel}>현재 메뉴</h1>
             </div>
-            <button className={styles.button}>
-              <div className={styles.label}>메뉴 순서 변경</div>
+            <button className={styles.buttonWrapper}>
+              <div className={styles.buttonLabel}>메뉴 순서 변경</div>
             </button>
           </div>
-          {menuData.map((menuCategory) => (
-            <div
-              key={menuCategory.category_name}
-              className={styles.layout}
-              id="category_card"
-            >
-              <div className={styles.titlearea1} id="category_card_title">
-                <h2 className={styles.title1}>{menuCategory.category_name}</h2>
-                <button className={styles.icon3}>
-                  <button className={styles.xS}>
+          {menuData.map((menuCategory, index) => (
+            <div key={index} className={styles.layout} id="category_card">
+              <div
+                className={styles.categoryTitleWrapper}
+                id="category_card_title"
+              >
+                <h2 className={styles.categoryTitleLabel}>
+                  {menuCategory.category_name}
+                </h2>
+                <button className={styles.iconWrapper}>
+                  <div className={styles.icon}>
                     <img alt="" src={deleteIcon} />
-                  </button>
+                  </div>
                 </button>
               </div>
-
-              {menuCategory.main_menus.map((menuItem) => (
-                <div
-                  key={menuItem.id}
-                  className={styles.row}
-                  id="menu_card_row"
-                >
-                  <div className={styles.menuCard} id="menu_card_body">
-                    <div className={styles.layout1} id="menu_card_buttons">
-                      <div className={styles.icongroup}>
-                        <button className={styles.icon3}>
-                          <button className={styles.xS}>
+              <div className={styles.menuRow} id="menu_card_row">
+                {menuCategory.main_menus.map((menuItem, index) => (
+                  <div
+                    className={
+                      menuItem.menu_status
+                        ? styles.menuCard
+                        : styles.menuCardInactive
+                    }
+                    key={index}
+                    id="menu_card_body"
+                  >
+                    <div
+                      className={styles.menuCardLayout}
+                      id="menu_card_buttons"
+                    >
+                      {!menuItem.menu_status && (
+                        <div
+                          className={styles.InactiveStatusBadge}
+                          id="menu_stop"
+                        >
+                          <b className={styles.InactiveStatusLabel}>
+                            메뉴 중지
+                          </b>
+                        </div>
+                      )}
+                      <div
+                        className={
+                          menuItem.menu_status
+                            ? styles.iconGroupWrapper
+                            : styles.iconGroupWrapperInactive
+                        }
+                      >
+                        <button className={styles.iconWrapper}>
+                          <div className={styles.icon}>
                             <img alt="" src={editIcon} />
-                          </button>
+                          </div>
                         </button>
-                        <button className={styles.icon3}>
-                          <button className={styles.xS}>
-                            <img alt="" src={pauseIcon} />
-                          </button>
+                        <button className={styles.iconWrapper}>
+                          <div
+                            className={styles.icon}
+                            onClick={() => {
+                              const confirmMessage = menuItem.menu_status
+                                ? "메뉴를 중지하시겠습니까?"
+                                : "메뉴를 재개하시겠습니까?";
+
+                              if (window.confirm(confirmMessage)) {
+                                onStatusButtonClick(
+                                  menuItem.id,
+                                  menuItem.menu_status
+                                );
+                              }
+                            }}
+                          >
+                            <img
+                              alt=""
+                              src={menuItem.menu_status ? pauseIcon : playIcon}
+                            />
+                          </div>
                         </button>
-                        <button className={styles.icon3}>
-                          <button className={styles.xS}>
+                        <button className={styles.iconWrapper}>
+                          <div className={styles.icon}>
                             <img alt="" src={deleteIcon} />
-                          </button>
+                          </div>
                         </button>
                       </div>
                     </div>
 
-                    <div className={styles.layout2} id="menu_card">
-                      <img
-                        className={styles.menuimageIcon}
-                        alt=""
-                        src={menuItem.image_url}
-                      />
+                    <div
+                      className={
+                        menuItem.menu_status
+                          ? styles.menuInfoLayout
+                          : styles.menuInfoLayoutInactive
+                      }
+                      id="menu_card"
+                    >
+                      {menuItem.image_url && (
+                        <img
+                          className={
+                            menuItem.menu_status
+                              ? styles.menuImage
+                              : styles.menuImageInactive
+                          }
+                          alt=""
+                          src={menuItem.image_url}
+                          loading="lazy"
+                        />
+                      )}
 
-                      <div className={styles.menuinfo} id="menu_card_text">
-                        <div className={styles.div}>
-                          <h1 className={styles.menuname} id="menu_name">
+                      <div
+                        className={
+                          menuItem.menu_status
+                            ? styles.menuContent
+                            : styles.menuContentInactive
+                        }
+                        id="menu_card_text"
+                      >
+                        <div className={styles.menuInfo}>
+                          <h1 className={styles.menuName} id="menu_name">
                             {menuItem.name}
                           </h1>
 
-                          <h2 className={styles.description} id="menu_explain">
-                            {menuItem.description}
-                          </h2>
+                          {menuItem.description && (
+                            <h2
+                              className={styles.menuDescription}
+                              id="menu_explain"
+                            >
+                              {menuItem.description}
+                            </h2>
+                          )}
                         </div>
 
-                        <h2 className={styles.value1} id="menu_cost">
-                          {menuItem.price}원
+                        <h2 className={styles.menuPrice} id="menu_cost">
+                          {menuItem.price.toLocaleString()}원
                         </h2>
                       </div>
                     </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           ))}
         </div>
-        <footer className={styles.layout22} id="current_menu_down">
-          <button className={styles.floatingbutton}>
-            <div className={styles.floatingbuttonChild} />
-            <img className={styles.plusLIcon} alt="" src={plusIcon} />
+        <div className={styles.floatingButtonWrapper} id="current_menu_down">
+          <button className={styles.floatingButton}>
+            <div className={styles.floatingButtonChild} />
+            <img className={styles.plusIcon} alt="" src={plusIcon} />
           </button>
-        </footer>
+        </div>
       </div>
     </>
   );
